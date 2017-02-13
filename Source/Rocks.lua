a08a8d4ee3d083112529b624d3dca13174ce2b79
b015133f <b015133f>
2017-02-13 22:38:56 +0000

    Rocks now spawn based on time

--- Source/Rocks.lua
+++ Source/Rocks.lua
@@ -2,13 +2,13 @@ function RocksLoad()
   
   require "Source/CollisionHandler"
   require "Source/Player"
-  
-  numberOfRocks = 5
+  require "Source/MainGame"
   
   rocks = {}
-  for i=1, numberOfRocks do
-    table.insert(rocks, {image = love.graphics.newImage("Sprites/snowball.png"), x = math.random(1,400), y = -1 * math.random(100,400), width = 64, height = 64, speed = 960 })
-  end
+  rockImage = love.graphics.newImage("Sprites/Snowball.png")
+  rockSpawnTime = 10
+  table.insert(rocks, {image = rockImage, x = math.random(1,400), y = -1 * math.random(100,400), width = 64, height = 64, speed = 960 })
+
     
 end
 
@@ -21,18 +21,26 @@ function RocksUpdate(dt)
       r.x = math.random(50, 400)
       r.speed = fallingSpeed * (0.1 * math.random(20, 50))
     end
+    
+    if BoxCollision(r.x, r.y, r.width, r.height, playerPosX, playerPosY, playerWidth, playerHeight) == true then
+      gameState = "death"
+      for i=table.maxn(rocks), 1 do
+        table.remove(rocks, i)
+      end
+      table.insert(rocks, {image = rockImage, x = math.random(1,400), y = -1 * math.random(100,400), width = 64, height = 64, speed = 960 })
+    end
+    
   end
   
-  
+  if math.ceil(timer/rockSpawnTime) > table.maxn(rocks) then
+    table.insert(rocks, {image = rockImage, x = math.random(1,400), y = -1 * math.random(100,400), width = 64, height = 64, speed = fallingSpeed * (0.1 * math.random(20, 50)) })
+  end
   
 end
 
 function RocksDraw()
+  
   for i,r in ipairs(rocks) do
-    if BoxCollision(r.x, r.y, r.width, r.height, playerPosX, playerPosY, playerWidth, playerHeight) == true then
-      love.graphics.setColor(255, 0, 0)
-      love.graphics.print("Kill", 100, 100)
-    end
     love.graphics.draw(r.image, r.x, r.y, 0, 0.25, 0.25)
   end
   
