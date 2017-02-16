ba3327a8e4ae1b1ba00ecf9d0487c6f6aad0c5ce
STUDENT\o017293f <lucasjamescarter@yahoo.co.uk>
2017-02-16 17:37:08 +0000

    Finished and tested shop code. Includes the two placeholder button images used within the code currently.

--- Source/Shop.lua
+++ Source/Shop.lua
@@ -1,37 +1,33 @@
+mouse = {}
 
-
-function love.load()    
+function shopload()    
   
-  money = 1000
+  money = 500
   
   upgrade1Bought = false
   upgrade2Bought = false
   
-
-  if upgrade2Bought == false then
-    buyButton2 = love.graphics.newImage("sprites/blankLongButton.png") -- change to buy button image + placeholder button image
-  end  
-  if (upgrade1Bought == true or money <= 500) then
-    boughtButton1 = love.graphics.newImage("sprites/upgradeBoughtButton.png") -- placeholder button image  
-    else
-    buyButton1 = love.graphics.newImage("sprites/blankLongButton.png") -- change to buy button image + placeholder button image
-  end
-  if upgrade2Bought == true then
-    boughtButton2 = love.graphics.newImage("sprites/upgradeBoughtButton.png") -- placeholder button image
-  end
+  buyButton2 = love.graphics.newImage("sprites/blankLongButton.png") -- change to buy button image + placeholder button image
+    
+  boughtButton1 = love.graphics.newImage("sprites/upgradeBoughtButton.png") -- placeholder button image  
+    
+  buyButton1 = love.graphics.newImage("sprites/blankLongButton.png") -- change to buy button image + placeholder button image
+    
+  boughtButton2 = love.graphics.newImage("sprites/upgradeBoughtButton.png") -- placeholder button image
+ 
   
   buyButton1PosX = 222/2
   buyButton1PosY = 600/2  
   buyButton2PosX = 222/2
   buyButton2PosY = 800/2
   boughtButton1PosX = 222/2
-  boughtButton1PosY = 600/2  
+  boughtButton1PosY = 600/2           -- Problem! Can only click one button! Fix!
   boughtButton2PosX = 222/2
   boughtButton2PosY = 800/2
   
-end
+  end
 
-function love.update ()
+function shopupdate ()
   mouseX = love.mouse.getX()
   mouseY = love.mouse.getY()
   
@@ -39,18 +35,22 @@ function love.update ()
     checkBuy2 = CheckCollision(mouseX, mouseY, 1, 1, buyButton2PosX, buyButton2PosY, 144, 64)    
 end
 
-function love.draw()
-  game_screen()
-   if main == true then        
+function shopdraw()
+  --game_screen()
+   if main == false then        
     love.graphics.draw(buyButton1, buyButton1PosX, buyButton1PosY)
     love.graphics.draw(buyButton2, buyButton2PosX, buyButton2PosY)
-    love.graphics.draw(boughtButton1, boughtButton1PosX, boughtButton1PosY)
-    love.graphics.draw(boughtButton2, boughtButton2PosX, boughtButton2PosY)
+    if upgrade1Bought == true then
+      love.graphics.draw(boughtButton1, boughtButton1PosX, boughtButton1PosY)
+    end
+    if upgrade2Bought == true then
+      love.graphics.draw(boughtButton2, boughtButton2PosX, boughtButton2PosY)
+    end
   end
 end
 
-function love.mousepressed(mx, my, button)
-  if (button == 1 and checkBuy1 and money >= 500 and upgrade1Bought == false) then    
+function Mousepressed(mx, my, button)
+  if (button == 1 and checkBuy1 and money >= 500 and upgrade1Bought == false) then  
     upgrade1Bought = true
     money = money - 500      
   end
