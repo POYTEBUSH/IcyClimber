6dadf6afa75d87681238e2594cbdc2d5bef387a3
Peter <poytebush@gmail.com>
2017-03-16 23:05:31 +0000

    UI Updates

--- Source/GameUI.lua
+++ Source/GameUI.lua
@@ -1,6 +1,6 @@
 function GameUILoad()
   score = 0
-  snow = love.graphics.newImage("Sprites/SnowFloor.png") 
+  scoreBar = love.graphics.newImage("Sprites/ScoreBar.png") 
   
   slowButton = love.graphics.newImage("Sprites/SlowDownButton.png")
   slowButtonNoHand = love.graphics.newImage("Sprites/SlowDownButtonNoHand.png")
@@ -19,7 +19,7 @@ end
 
 function GameUIDraw()
   love.graphics.setColor(255, 255, 255)
-  love.graphics.draw(snow, snowPanelX, snowPanelY) 
+  love.graphics.draw(scoreBar, snowPanelX, snowPanelY) 
   
   if slowDown == true then
     love.graphics.draw(slowButton, slowButtonX, slowButtonY, 0, 0.4, 0.4)
@@ -27,22 +27,20 @@ function GameUIDraw()
   
   if speedModifier == 0.5 then
     love.graphics.draw(slowButtonNoHand, slowButtonX, slowButtonY, 0, 0.4, 0.4)
-    love.graphics.draw(clockHand, slowButtonX + 40, slowButtonY + 40, (timer - speedModifierEnd + 40.2) / 1.6, 0.4, 0.4, 100, 100)
-    
+    love.graphics.draw(clockHand, slowButtonX + 40, slowButtonY + 40, (timer - speedModifierEnd + 40.2) / 1.6, 0.4, 0.4, 100, 100)    
   end
   
   love.graphics.setColor(0,0,0)
-  love.graphics.print("Score: " .. score, 50, windowHeight - 96, 0, 2, 2)
+  love.graphics.print("Score: " .. score, 20, windowHeight - 65, 0, 2, 2)
   
   if shield == true then
-    love.graphics.print("Shield" , 270, windowHeight - 96, 0, 2, 2)
+    love.graphics.print("Shield" , 270, windowHeight - 100, 0, 2, 2)
   end
   
-  love.graphics.print("Coins: " .. coins, 50, windowHeight - 48, 0, 2, 2)
+  love.graphics.print("Coins: " .. coins, 20, windowHeight - 40, 0, 2, 2)
   
   love.graphics.setColor(255,255,255)
 end
-
 
 function GameUIMousePressed(x, y, button)
   if slowDown == true then
